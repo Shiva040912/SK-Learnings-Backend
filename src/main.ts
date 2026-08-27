f905eff12cfe7db216352d9c09c8ab7eb2009f54
Shiva040912 <shivaarun0071@gmail.com>
2026-08-27 17:52:56 +0530

    link added

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
         'http://localhost:5173',
         'https://sk-learning-frontend.vercel.app',
         'https://sk-learning-frontend.shivaarun0071.workers.dev',
+        'https://sk-learnings-frontend.web.app',
       ];
 
       if (!origin) {
